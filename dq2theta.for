75ba382f1005ae0880238bc0b003d441ae0529ba
atowneND <atowne@nd.edu>
2015-06-11 13:03:42 -0400

    update so dq2theta reads dqcalc, same output

--- dq2theta.for
+++ dq2theta.for
@@ -5,6 +5,9 @@
 *     function of angle by evaluating the Legendre sum
 *           f(theta)  = sum d(q) * pq(cos theta)
 
+*     This version ignores comment lines (begining with #) in the input
+*     except for extracting j and jp from one comment line.
+*     The input file must be EXACTLY the output file produced by dqcalc
 
       implicit     none
       integer      in,out,j,jp,count,nchar,qend,i,QMAX,q,ntheta
@@ -16,6 +19,7 @@
 
 *     calls
       integer      GetLine
+      character    fnbc     ! returns first nonblank character of string
       external     abort
 
       call stdio (in,out)
@@ -24,12 +28,27 @@
 *     read the input file and echo the list of d(q).  check that q runs from
 *     0 to 2 * min(j,jp)
 
-      read (in,*) j,jp,dzero
+!     read (in,*) j,jp,dzero
+!     new section to scan through comments to find j and jp
+      DO WHILE (GetLine(in,line,nchar).gt.0)
+         i = index(line,'j and jp are')
+         IF (i.gt.0) THEN
+            read (line(i+12:),*) j,jp
+            go to 10
+         END IF
+      END DO
+10    continue
+      ! now skip 1 header line plus 2*jmin+1 values of Blambda (all commented)
+      DO i=1,2*min(j,jp)+2
+         nchar = GetLine(in,line,nchar)
+      END DO
+ 
       qend = 2*min(j,jp)
       count = -1
       DO WHILE (GetLine(in,line,nchar).gt.0)
          count = count + 1
-         read (line,*) i,tmp
+         read (line,*) i,x,tmp   !normalized dq is in third column
+         if (i.eq.0) dzero = x
          IF (i.eq.count .and. count.le.QMAX) THEN
             d(count) = tmp
          ELSE
@@ -45,7 +64,7 @@
 
       thdegmin=0.d0
       thdegmax=180.d0
-      thdegdel=0.d5
+      thdegdel=2.d0
 
       ntheta = nint( (thdegmax-thdegmin)/thdegdel )
       thdegdel = (thdegmax-thdegmin)/dble(ntheta)

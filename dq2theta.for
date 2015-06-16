cb7e4d1b882cfbab2563ddb673480b280760e5b1
atowneND <atowne@nd.edu>
2015-06-16 10:14:03 -0400

    dq2theta output modified

--- dq2theta.for
+++ dq2theta.for
@@ -9,16 +9,22 @@
 *     except for extracting j and jp from one comment line.
 *     The input file must be EXACTLY the output file produced by dqcalc
 
+*     This version writes the coefficients Blambda(j,jp) to a file
+*     named Blam_j_jp.  .
+
       implicit     none
-      integer      in,out,j,jp,count,nchar,qend,i,QMAX,q,ntheta
-      parameter    (QMAX=60)
+      integer      in,out,j,jp,count,nchar,qend,i,QMAX,q,ntheta,blamfile
+     -             ,ix,LAMMAX,lam,lammin
+      parameter    (QMAX=200,LAMMAX=200)
       double precision d(0:QMAX),P(0:QMAX),tmp, x,PI,thdeg,thdegmin,
-     -             thdegmax,thdegdel,ftheta,dzero
+     -             thdegmax,thdegdel,ftheta,dzero,thx,sumBlam,thxsave
+      double precision Blamda(0:LAMMAX),lambda(0:LAMMAX),angle(0:LAMMAX)
       parameter    (PI = 4.d0*atan(1.0d0))
       character    line*80
+      logical      :: found = .false.
 
 *     calls
-      integer      GetLine
+      integer      GetLine,fassign
       character    fnbc     ! returns first nonblank character of string
       external     abort
 
@@ -38,11 +44,29 @@
          END IF
       END DO
 10    continue
-      ! now skip 1 header line plus 2*jmin+1 values of Blambda (all commented)
-      DO i=1,2*min(j,jp)+2
+
+*     now construct a file name that includes j and jp
+      write (line,'("Blam_",i2.2,"_",i2.2,".dat")') j,jp
+      IF(fassign(blamfile,trim(line),2).gt.0) THEN
+         write (*,*) 'Blamda will be written to '//trim(line)
+      ELSE
+         call abort('problem opening file for Blambda')
+      END IF
+
+*     skip 1 header line, read 2*jmin+1 values of Blambda (all commented),
+      nchar = GetLine(in,line,nchar)
+      !write(blamfile,*)line(1:nchar)
+      tmp = 0.d0   ! tmp will accumulate sum of (2*lam+1)*Blam
+      DO i=1,2*min(j,jp)+1
          nchar = GetLine(in,line,nchar)
+         ix = index(line,'#')+1
+         read (line(ix:),*) lam,Blamda(lam),angle(lam)
+         tmp = tmp + dble(2*lam+1)*Blamda(lam)
+         !write(blamfile,*) line(ix:nchar)
       END DO
- 
+      sumBlam = tmp    ! will use this later
+      write (*,*) 'sum (2*lam+1)*Blam = ',sumBlam
+      
       qend = 2*min(j,jp)
       count = -1
       DO WHILE (GetLine(in,line,nchar).gt.0)
@@ -58,16 +82,18 @@
       if (count.ne.qend) call abort ('problem: wrong number of d(q)')
       write (out,*) '# List of d(q) for j, j'' = ', j,jp
       write (out,'('' # '',i4,f12.8  )') (q,d(q),q=0,qend)
-
+ 
+ 
 *     loop over theta.  for the present use simple values to loop
 *     from theta=0 to 180 degrees in two degree increments.
 
       thdegmin=0.d0
       thdegmax=180.d0
-      thdegdel=2.d0
+      thdegdel=0.5d0
 
       ntheta = nint( (thdegmax-thdegmin)/thdegdel )
       thdegdel = (thdegmax-thdegmin)/dble(ntheta)
+      thx = -1.d0
       P(0) = 1.d0
       DO i=0,ntheta
          thdeg = thdegmin + dble(i)*thdegdel
@@ -80,7 +106,31 @@
             ftheta = ftheta + dble(2*q+3)*d(q+1)*P(q+1) ! 2q+3 = 2(q+1)+1
          END DO
          ftheta = dzero*ftheta/sqrt(dble((2*j+1)*(2*jp+1)))
-         write(out,'(f10.1,f15.7)') thdeg, ftheta
+         write(out,'(f10.1,2f15.7)') thdeg, ftheta,
+     -      ftheta*sin(thdeg*PI/180.d0)
+         ! check if we've passed the first max of ftheta*sin(theta)
+         tmp = ftheta * sin(thdeg*PI/180.d0)
+         IF (.not.found .and. tmp.lt.thx) THEN
+            write (*,*) 'first max found at ',thdeg-thdegdel
+            write (*,'(f15.7,'' was first max'')') thx
+            thxsave = thx
+            found = .true.
+         ELSE
+            thx = tmp
+         END IF
       END DO
+
+*     write Blamda to blamfile and include normalized Blamda such that
+*     (2*lam_min + 1) * B(lam_min) = thx
+      lammin = abs(j-jp)
+      if (lammin.eq.0) call abort('oops -- do not choose j = jp')
+      tmp = thxsave/dble(2*lammin+1)/Blamda(lammin)
+      write (blamfile,*)'# lam  B-lambda(j,jp)  angle    '//
+     -                          ' normed B-lam*(2*lam+1)'
+      DO lam = abs(j-jp),j+jp
+         write (blamfile,'(i5,2f12.4,es16.6)')
+     -    lam, Blamda(lam), angle(lam), Blamda(lam)*tmp*dble(2*lam+1)
+      END DO
+ 
 
       end

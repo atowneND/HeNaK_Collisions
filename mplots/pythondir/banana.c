33e88c51678198c5c3d2139c1471e203f2e419b2
atowneND <atowne@nd.edu>
2015-07-14 16:25:06 -0400

    presentation mostly done

---
+++ mplots/pythondir/banana.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for (i = 0; i< 1001; i++)
+    {
+        if (i % 2 == 0)
+        {
+            printf("%i\n", i);
+        }
+    }
+
+         
+    return 0;
+}
